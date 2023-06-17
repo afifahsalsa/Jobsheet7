52c8e18e26ee60cf5820c33ae7fe9b4b21da8b3f
= <afifahsalsa.billa2@gmail.com>
2023-06-17 19:07:21 +0700

    css

--- resources/views/mahasiswa/nilai.blade.php
+++ resources/views/mahasiswa/nilai.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-12 margin-tb">
+        <div class="col-lg-12 margin-tb text-center">
             <div class="pull-left mt-2">
                 <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
             </div>
-            <div class="pull-left mt-2" >
+            <div class="pull-left mt-2 text-center" >
                 <h1>KARTU HASIL STUDI</h1>
             </div>
         </div>
